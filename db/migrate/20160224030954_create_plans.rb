035e5c7ad2bc1926f7106f4f368846e6c7e25f12
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2016-02-24 13:09:35 +0900

    Generate model plan

---
+++ db/migrate/20160224030954_create_plans.rb
@@ -0,0 +1,11 @@
+class CreatePlans < ActiveRecord::Migration
+  def change
+    create_table :plans do |t|
+      t.integer :user_id
+      t.integer :price
+      t.datetime :order_at
+      t.datetime :delivery_at
+      t.timestamps null: false
+    end
+  end
+end
