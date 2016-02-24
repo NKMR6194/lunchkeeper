0e6c1bca296791b16d1d1121b8f0e4002f91960d
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2016-02-24 13:11:47 +0900

    Generate model menu

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,4 @@
 class Shop < ActiveRecord::Base
 	has_secure_password
-
+	has_many :menus
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224021514) do
+ActiveRecord::Schema.define(version: 20160224031211) do
+
+  create_table "menus", force: :cascade do |t|
+    t.integer  "shop_id"
+    t.string   "name"
+    t.integer  "price"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "price"
+    t.datetime "order_at"
+    t.datetime "delivery_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string   "email"
