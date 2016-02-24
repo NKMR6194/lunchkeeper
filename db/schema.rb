097d364edd1fd88cf2377ea1045b7b49339f9909
kazukitash <kokuikki@gmail.com>
2016-02-24 20:51:37 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224104716) do
+ActiveRecord::Schema.define(version: 20160224112540) do
 
   create_table "menus", force: :cascade do |t|
     t.integer  "shop_id"
@@ -79,6 +79,11 @@ ActiveRecord::Schema.define(version: 20160224104716) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "phone"
+    t.string   "pref"
+    t.string   "city"
+    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
