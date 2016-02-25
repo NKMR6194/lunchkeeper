e21d4b816bafa7c26eae36c863dcc9d7f6802d1e
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2016-02-25 19:37:38 +0900

    メソッドをモジュールに分離

---
+++ app/controllers/concerns/position.rb
@@ -0,0 +1,28 @@
+module Position
+  def fetch uri_str, limit = 3
+    raise ArgumentError, "HTTP redirect too deep" if limit == 0
+
+    # response = Net::HTTP.get_response(URI.parse(uri_str))
+    uri_str = URI.encode(uri_str)
+    response = Net::HTTP.get_response(URI.parse(uri_str))
+    case response
+    when Net::HTTPSuccess
+      response
+    when Net::HTTPRedirection
+      fetch(response["location"], limit - 1)
+    else
+      response.value
+    end
+  end
+
+  def find_position(full_address)
+    request = "http://maps.googleapis.com/maps/api/geocode/json?address=#{full_address}"
+    response = fetch(request)
+    temp = response.body
+    temp = JSON.parse(temp)
+
+    lat = temp["results"][0]["geometry"]["location"]["lat"].to_f
+    lot = temp["results"][0]["geometry"]["location"]["lng"].to_f
+    return lat, lot
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_shop!
   before_action :set_user
 
+  include Position
+
   def show
     @active_plan = Plan.find_by(user_id: @user.id, state: Plan.states[:contracting])
     @old_plans   = Plan.where(user_id: @user.id, state: Plan.states[:done]).limit(10)
@@ -31,32 +33,5 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :phone, :pref, :city, :address)
-  end
-
-  def fetch uri_str, limit = 3
-    raise ArgumentError, "HTTP redirect too deep" if limit == 0
-
-    # response = Net::HTTP.get_response(URI.parse(uri_str))
-    uri_str = URI.encode(uri_str)
-    response = Net::HTTP.get_response(URI.parse(uri_str))
-    case response
-    when Net::HTTPSuccess
-      response
-    when Net::HTTPRedirection
-      fetch(response["location"], limit - 1)
-    else
-      response.value
-    end
-  end
-
-  def find_position(full_address)
-    request = "http://maps.googleapis.com/maps/api/geocode/json?address=#{full_address}"
-    response = fetch(request)
-    temp = response.body
-    temp = JSON.parse(temp)
-
-    lat = temp["results"][0]["geometry"]["location"]["lat"].to_f
-    lot = temp["results"][0]["geometry"]["location"]["lng"].to_f
-    return lat, lot
   end
 end
