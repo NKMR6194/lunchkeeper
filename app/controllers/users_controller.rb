73164af08248279e5734ade440a87f207732cdae
kazukitash <kokuikki@gmail.com>
2016-02-25 17:10:00 +0900

    fix user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_shop!
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user
 
   def show
     @active_plan = Plan.find_by(user_id: @user.id, state: :contracting)
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to user_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
