16e1a78d23599a22a338c4ca8c083972e119f061
SAKATA Sinji <contact@sinjis-view.mydns.jp>
2016-02-25 19:32:22 +0900

    ユーザー情報保存時に経度緯度を取得する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,9 @@ class UsersController < ApplicationController
 
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      address = user_params[:pref] + user_params[:city] + user_params[:address]
+      lat, lot = find_position(address)
+      if @user.update(user_params) && @user.update(latitude: lat, longitude: lot)
         format.html { redirect_to user_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -29,5 +31,32 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :phone, :pref, :city, :address)
+  end
+
+  def fetch uri_str, limit = 3
+    raise ArgumentError, "HTTP redirect too deep" if limit == 0
+
+    # response = Net::HTTP.get_response(URI.parse(uri_str))
+    uri_str = URI.encode(uri_str)
+    response = Net::HTTP.get_response(URI.parse(uri_str))
+    case response
+    when Net::HTTPSuccess
+      response
+    when Net::HTTPRedirection
+      fetch(response["location"], limit - 1)
+    else
+      response.value
+    end
+  end
+
+  def find_position(full_address)
+    request = "http://maps.googleapis.com/maps/api/geocode/json?address=#{full_address}"
+    response = fetch(request)
+    temp = response.body
+    temp = JSON.parse(temp)
+
+    lat = temp["results"][0]["geometry"]["location"]["lat"].to_f
+    lot = temp["results"][0]["geometry"]["location"]["lng"].to_f
+    return lat, lot
   end
 end
