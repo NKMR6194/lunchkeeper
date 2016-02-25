67cfb912f84473a5b4fc381e1e443b6af50b190b
kazukitash <kokuikki@gmail.com>
2016-02-25 17:01:25 +0900

    modify redirect_path

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_shop!
 
   def index
-    redirect_to plans_path if user_signed_in?
-    redirect_to shop_path  if shop_signed_in?
+    redirect_to user_path if user_signed_in?
+    redirect_to shop_path if shop_signed_in?
   end
 end
