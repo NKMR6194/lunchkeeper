19293b55e4f05ffa8c0aaf28b726fce77a892709
kazukitash <kokuikki@gmail.com>
2016-02-26 09:23:12 +0900

    design

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -2,4 +2,10 @@ module ShopsHelper
   def operation_time shop
     "#{shop.open_at.strftime("%H:%M")} 〜 #{shop.close_at.strftime("%H:%M")}"
   end
+
+  def shop_sample shops
+    text = shops[0..2].map{ |s| s.name }.join("、")
+    text << " 他" if shops.size > 3
+    text << " 合計#{shops.size}店"
+  end
 end
